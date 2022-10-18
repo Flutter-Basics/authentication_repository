f160ff60a67df0a8cafb9e1ae3d90f4970e3931f
soomrozaid <soomrozaid@gmail.com>
2022-10-18 00:29:31 -0600

    shared_preference debugging

--- lib/src/authentication_repository.dart
+++ lib/src/authentication_repository.dart
@@ -12,12 +12,25 @@ class AuthenticationRepository {
     try {
       SharedPreferences _sharedPref = await SharedPreferences.getInstance();
       List<String> _users = _sharedPref.getStringList('users') ?? <String>[];
+      print('--- Debugging ---');
+      print('');
+      print(_users);
+      print('');
+      print(' --- ');
+      print('');
 
       Map<String, String> _user = {
         'id': const Uuid().v4(),
         'email': email,
         'password': password
       };
+      print(_user);
+      print('');
+      print(' --- ');
+      print('');
+      print(jsonEncode(_user));
+      print('');
+      print(' --- End --- ');
 
       _sharedPref.setStringList('users', [..._users, jsonEncode(_user)]);
 
@@ -40,8 +53,23 @@ class AuthenticationRepository {
       List<Map<String, String>> _usersDecoded =
           _users.map((e) => jsonDecode(e) as Map<String, String>).toList();
 
+      print('--- Debugging ---');
+      print(_users);
+      print('');
+      print(' --- ');
+      print('');
+      print(_usersDecoded);
+      print('');
+      print(' --- ');
+      print('');
+
       Map<String, String> _user =
           _usersDecoded.firstWhere((element) => element['email'] == email);
+
+      print(_user);
+      print('');
+      print(' --- End --- ');
+
       if (_user.isNotEmpty) {
         await Future.delayed(const Duration(seconds: 5));
         return true;
